9936ae2be90d98ae570e6685c332c58405fbf105
tinkivink1 <92641773+tinkivink1@users.noreply.github.com>
2022-07-21 15:53:35 +0300

    Добавьте файлы проекта.

---
+++ client/Program.cs
@@ -0,0 +1,151 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+const int DGRAM_DATA_SIZE = 32768;
+
+string IP = "127.0.0.1";
+int tcpPort = 44445;
+int udpPort = 44444;
+string filename = "Test_File.txt";
+int timeout = 500;
+// Данные из файла 
+// File data 
+byte[] data;
+
+//try
+//{
+//    IP = args[0];
+//    tcpPort = Int32.Parse(args[1]);
+//    udpPort = Int32.Parse(args[2]);
+//    filename = args[3];
+//    timeout = Int32.Parse(args[4]);
+
+//    if (!File.Exists(filename))
+//    {
+//        Console.WriteLine("Specified file do not exists");
+//    }
+
+//    // Проверка на опасные символы в пути/названии файла 
+//    // Check for dangerous symbols in filename/file path
+//    // ? , : " * > < |
+//    if (Regex.IsMatch(filename, "(|\\?|\\,|:|\"|\\*|>|<|\\||)"))
+//    {
+//        Console.WriteLine("Invalid filename or file path");
+//    }
+//}
+//catch
+//{
+//    Console.WriteLine("For the correct operation of the program\n" +
+//                    "the following arguments must be entered:\n" +
+//                    "1. IP\n" +
+//                    "2. TCP Port\n" +
+//                    "3. UDP Port\n" +
+//                    "4. Filename\n" +
+//                    "5. TCP connection timeout\n" +
+//                    "Execution aborted");
+//    return -1;
+//}
+
+
+
+// Установление соединения
+// Connection estblishing
+TcpClient tcpClient = new TcpClient(IP, tcpPort);
+UdpClient udpClient = new UdpClient(IP, udpPort);
+BaseInfo baseInfo = new BaseInfo(filename, udpPort);
+TimeSpan responeTime = TimeSpan.FromMilliseconds(timeout);
+MarkedDatagram markdgram;
+
+if (!tcpClient.Connected)
+{
+    Console.WriteLine("No connection");
+}
+
+var tcpStream = tcpClient.GetStream();
+var serializedBaseInfo = Serialize<BaseInfo>(baseInfo);
+tcpStream.Write(serializedBaseInfo);
+// Отправка файла
+// File sending
+tcpStream.Close();
+data = ReadFile(filename);
+Console.WriteLine(Math.Floor((double)data.Length / DGRAM_DATA_SIZE));
+
+byte[] serializedDatagram;
+for (int i = 0; i < Math.Floor((double)data.Length / DGRAM_DATA_SIZE) - 1; i++)
+{
+    markdgram = new MarkedDatagram(i, data
+                                        .Skip(i * DGRAM_DATA_SIZE)
+                                        .Take(DGRAM_DATA_SIZE)
+                                        .ToArray());
+    serializedDatagram = Serialize<MarkedDatagram>(markdgram);
+    udpClient.Send(serializedDatagram, serializedDatagram.Length);
+
+    //Task<bool> sendingConfirmation = Task.Run(() => SendingConfirmation(i));
+    //if (!sendingConfirmation.Wait(responeTime))
+    //{
+    //    i--;
+    //    Console.WriteLine("12321");
+    //    continue;
+    //}
+    //if (!sendingConfirmation.Result)
+    //{
+    //    i--;
+    //    Console.WriteLine("32123");
+    //    continue;
+    //}
+}
+
+return 0;
+
+bool SendingConfirmation(int i)
+{
+    byte[] buffer = new byte[DGRAM_DATA_SIZE];
+    tcpStream.Read(buffer, 0, buffer.Length);
+    if (BitConverter.ToInt32(buffer) == i)
+        return true;
+    else
+        return false;
+}
+
+byte[] ReadFile(string filename)
+{
+    return Encoding.UTF8.GetBytes(File.ReadAllText(filename));
+}
+
+
+static byte[] Serialize<T>(T data)
+    where T : class
+{
+    return Encoding.UTF8.GetBytes(JsonSerializer.Serialize<T>(data));
+}
+
+class MarkedDatagram
+{
+    public int id { get; set; }
+    public byte[]? data { get; set; }
+
+    public MarkedDatagram(int id, byte[] data)
+    {
+        this.id = id;
+        this.data = data;
+    }
+}
+
+class BaseInfo
+{
+    public string filename { get; set; }
+    public int udpPort { get; set; }
+
+    public BaseInfo(string filename, int udpPort)
+    {
+        this.filename = filename;
+        this.udpPort = udpPort;
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+const int DGRAM_DATA_SIZE = 32768;
+
+string IP = "127.0.0.1";
+int tcpPort = 44445;
+int udpPort;
+string path = ".";
+string filename;
+string data = ""; 
+byte[] buffer = new byte[DGRAM_DATA_SIZE];
+List<byte> fileData = new List<byte>();
+
+
+// Обработка параметров командной строки 
+// Command line parameters processing
+//try
+//{
+//    IP = args[0];
+//    tcpPort = Int32.Parse(args[1]);
+//    path = args[2];
+//}
+//catch
+//{
+//    Console.WriteLine("For the correct operation of the program\n" +
+//                    "the following arguments must be entered:\n" +
+//                    "1. IP\n" +
+//                    "2. TCP port\n" +
+//                    "3. File path\n" +
+//                    "Execution aborted");
+//    return -1;
+//}
+
+
+TcpListener server = new TcpListener(IPAddress.Parse(IP), tcpPort);
+server.Start();
+Console.WriteLine("Waiting for connection");
+var tcpClient = server.AcceptTcpClient();
+Console.WriteLine("Connection established");
+while (true)
+{
+    NetworkStream stream = tcpClient.GetStream();
+
+    //var bytesCount = stream.Read(buffer, 0, buffer.Length);
+    //Console.WriteLine(bytesCount);
+    int i;
+
+    // Получаем отправленные на сервер данные
+    // Loop to receive the data sent by the client.
+    while ((i = stream.Read(buffer, 0, buffer.Length)) != 0)
+    {
+        data += System.Text.Encoding.UTF8.GetString(buffer, 0, i);
+    }
+
+    Console.WriteLine(data);
+    // парсинг полученных данных (в формате json для удобства)
+    filename = JsonSerializer.Deserialize<BaseInfo>(data).filename;
+    udpPort = JsonSerializer.Deserialize<BaseInfo>(data).udpPort;
+
+    Console.WriteLine($"Filename: {filename} udp port: {udpPort}");
+    // Конечная точка и подключение
+    IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+    UdpClient udpClient = new UdpClient(udpPort);
+
+    while ((buffer = udpClient.Receive(ref endPoint)).Length != 0)
+    {
+        MarkedDatagram markedOne = JsonSerializer.Deserialize<MarkedDatagram>(Deserialize(buffer));
+        Console.WriteLine(DecodeBase64(markedOne.data));
+        //stream.Write(BitConverter.GetBytes(markedOne.id));
+    }
+
+    Console.WriteLine(fileData);
+    try
+    {
+        
+
+    }
+    catch (JsonException)
+    {
+        Console.WriteLine("Recieved arguments does not match the required ones");
+        return -1;
+    }
+    catch (ArgumentNullException)
+    {
+        Console.WriteLine("Recieved argument was null");
+        return -1;
+    }
+    catch (SocketException)
+    {
+        Console.WriteLine("Received combination \"IP/UDP Port\" does not exist");
+        return -1;
+    }
+
+    return 0;
+}
+
+string Deserialize(byte[] data)
+{
+    return Encoding.UTF8.GetString(data);
+}
+
+string DecodeBase64(string value)
+{
+    var valueBytes = Convert.FromBase64String(value);
+    return Encoding.UTF8.GetString(valueBytes);
+}
+
+class MarkedDatagram
+{
+    public int id { get; set; }
+    public string data { get; set; }
+
+    public MarkedDatagram(int id, string data)
+    {
+        this.id = id;
+        this.data = data;
+    }
+}
+
+class BaseInfo
+{
+    public string filename { get; set; }
+    public int udpPort { get; set; }
+
+    public BaseInfo(string filename, int udpPort)
+    {
+        this.filename = filename;
+        this.udpPort = udpPort;
+    }
+}
